961a261378ce3b37e97cdc44ccccfef476d69148
Anders <Walla>
2022-12-22 13:47:31 +0100

    Add logging to HealthCareSystem

--- RabbitMq_integration/HealthcareSystem/HealthCareSystem.cs
+++ RabbitMq_integration/HealthcareSystem/HealthCareSystem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
 
 namespace RabbitMq_integration.HealthcareSystem;
 
@@ -9,12 +10,15 @@ public interface IHealthCareSystem
 
 public class HealthCareSystem : IHealthCareSystem
 {
-	public HealthCareSystem()
-    {
-    }
+	private readonly ILogger<HealthCareSystem> _logger;
+
+	public HealthCareSystem(ILogger<HealthCareSystem> logger)
+	{
+		_logger = logger;
+	}
 
     public void CPUpdate(CommunicationParty.CommunicationParty communicationParty)
     {
-        Console.WriteLine("Test. Update to cpp");
+	    _logger.LogInformation("Health Care System received an update to communication party " + communicationParty.HerId);
     }
 }
