aeeed79f92f4fe1db38c488805038ce1601c1527
Anders <Walla>
2023-01-10 10:11:41 +0100

    Combine CommunicationPartyExportService with InitialPopulationJob
    - Also move InitialPopulationJob to own file

--- InitialPopulation/Program.cs
+++ InitialPopulation/Program.cs
@@ -1,16 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System;
-using System.Collections.Generic;
 using System.ServiceModel;
-using System.Threading;
-using System.Threading.Tasks;
 using InitialPopulation.ArExportService;
 using InitialPopulation.HealthcareSystem;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using RabbitMq_integration.ArExportService;
 
 namespace InitialPopulation;
 
@@ -24,7 +18,6 @@ class Program
                 //See InitialPopulation to see how to do a initial population of your health care system
                 services.AddSingleton<IARExportService>(CreateArExportService);
                 services.AddSingleton<IHealthCareSystem, HealthCareSystem>();
-                services.AddScoped<CommunicationPartyExportService>();
                 services.AddHostedService<InitialPopulationJob>();
             })
             .Build();
@@ -49,44 +42,5 @@ class Program
 	    client.ClientCredentials.UserName.UserName = ""; // Register platform user
 	    client.ClientCredentials.UserName.Password = "";
 	    return client;
-    }
-}
-
-internal class InitialPopulationJob :BackgroundService
-{
-    private readonly CommunicationPartyExportService _communicationPartyExportService;
-
-    private readonly IHealthCareSystem _healthCareSystem;
-    private readonly ILogger<InitialPopulationJob> _logger;
-
-    public InitialPopulationJob(CommunicationPartyExportService communicationPartyExportService, IHealthCareSystem healthCareSystem, ILogger<InitialPopulationJob> logger)
-    {
-	    _communicationPartyExportService = communicationPartyExportService;
-	    _healthCareSystem = healthCareSystem;
-	    _logger = logger;
-    }
-
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-		_logger.LogInformation("Initial population start...");
-	    await SyncAllCommunicationPartiesAsync(stoppingToken);
-	    _logger.LogInformation("Initial population end...");
-    }
-
-    private async Task SyncAllCommunicationPartiesAsync(CancellationToken stoppingToken)
-    {
-	    var comParties = _communicationPartyExportService.GetAllCommunicationPartiesXmlAsync(stoppingToken);
-	 
-	    await foreach (var comParty in comParties.WithCancellation(stoppingToken))
-	    {
-		    try
-		    {
-			    _healthCareSystem.CommunicationPartyUpdate(comParty);
-		    }
-		    catch (Exception e)
-		    {
-			    _logger.LogError(e, "Could not run initial population on communication party {HerId}", comParty.HerId);
-		    }
-	    }
     }
 }
