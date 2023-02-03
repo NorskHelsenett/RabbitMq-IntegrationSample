bbb78121d0139199eae5de47a77abd682a0df416
Vetle Aske <vetle.aske@nhn.no>
2023-02-03 12:02:33 +0100

    Fix connection loop

--- RabbitMq_Stream_integration/BackgroundServices/RabbitStreamConsumer.cs
+++ RabbitMq_Stream_integration/BackgroundServices/RabbitStreamConsumer.cs
@@ -1,19 +1,16 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System.Globalization;
-using System.Text.RegularExpressions;
 using RabbitMq_Stream_integration.CommunicationParty;
 using RabbitMq_Stream_integration.Configuration;
 using RabbitMq_Stream_integration.HealthcareSystem;
 using RabbitMQ.Stream.Client;
 using RabbitMQ.Stream.Client.Reliable;
 using Message = RabbitMQ.Stream.Client.Message;
-using RabbitMq_Stream_integration.AmqpInterop;
 
 namespace RabbitMq_Stream_integration.BackgroundServices;
 // This example is based on version 1.0.0
-public class RabbitStreamConsumer : BackgroundService
+public class RabbitStreamConsumer : BackgroundService, IDisposable
 {
     private readonly ILogger _logger;
     private readonly ILogger<Consumer> _consumeLogger;
@@ -43,7 +40,7 @@ public class RabbitStreamConsumer : BackgroundService
     private async Task StartListeningToSubscriptionAsync(CancellationToken stoppingToken)
     {
         // Subscription is created asynchronously, and may not be ready yet, so we need retry logic
-        while (!stoppingToken.IsCancellationRequested && await TrySetupConsumer(stoppingToken))
+        while (!stoppingToken.IsCancellationRequested && !await TrySetupConsumer(stoppingToken))
         {
             await Task.Delay(_retryDelay, stoppingToken);
         }
@@ -81,7 +78,7 @@ public class RabbitStreamConsumer : BackgroundService
             }
             catch (Exception e)
             {
-                _logger.LogError(e, "Kunne ikke hente offset");
+                _logger.LogError(e, "Could not receive the offset");
             }
 
             // Create a consumer
@@ -90,28 +87,28 @@ public class RabbitStreamConsumer : BackgroundService
                 {
                     Reference = reference,
                     // Consume the stream from the offest recived from the server
-                    OffsetSpec = trackedOffset == 0 ? new OffsetTypeLast() : new OffsetTypeOffset(trackedOffset),
+                    OffsetSpec = new OffsetTypeOffset(290), //trackedOffset == 0 ? new OffsetTypeOffset(287) : new OffsetTypeOffset(trackedOffset),
                     // Receive the messages
                     MessageHandler = async (sourceStream, consumer, ctx, message) =>
                     {
                         //Storing offest after each 100 message is consumed.
-                        if (++messagesConsumed != 0 && messagesConsumed % 100 == 0)
+                        if (++messagesConsumed != 0)
                         {
                             await consumer.StoreOffset(ctx.Offset);
+                            _logger.LogInformation("Stored offset: {ctx.Offset}", ctx.Offset);
                         }
 
                         await HandleMessage(sourceStream, message, messagesConsumed);
+                        await Task.CompletedTask;
                     }
                 }, _consumeLogger);
             
             // Handle application shutdown gracefully:
             stoppingToken.Register(() => CloseChannel(system, consumer));
-            
             return true;
         } catch (Exception e)
         {
             _logger.LogError(e, "Failed to set up connection to stream {_settings.QueueName} on RabbitMq. It may not have been created yet, will retry in a moment...", _settings.QueueName);
-
             await consumer?.Close()!;
             await system?.Close()!;
             return false;
@@ -133,10 +130,10 @@ public class RabbitStreamConsumer : BackgroundService
             // A Communication Party has been created or updated, Fetch the latest version so we can send it to the health care system
             
             var herId = props["herId"].ToString();
-            Console.WriteLine("Readed messageNr: " + messagesConsumed + ", with herId: " + herId);
+            Console.WriteLine("Read message: " + messagesConsumed + ", with herId: " + herId);
             
             int mseconds = rnd.Next(50,5000);
-            Thread.Sleep(mseconds); // Add random sleeptime to spread out load when a new event comes
+            Task.Delay(mseconds); // Add random sleeptime to spread out load when a new event comes
             var communicationParty = await _communicationPartyService.GetCommunicationPartyDetailsAsync(Int32.Parse(herId!));
 
             // Send the update to the health care system
@@ -147,7 +144,7 @@ public class RabbitStreamConsumer : BackgroundService
     private void CloseChannel(StreamSystem system, Consumer consumer)
     {
         // Allow already received messages some time to process before we tear down the channel:
-        Thread.Sleep(TimeSpan.FromSeconds(3));
+        Thread.Sleep(TimeSpan.FromSeconds(1));
 
         system?.Close();
         consumer?.Close();
