20a19c9d4251ce4d61ae579b3f52c0d27dbaf781
Vetle Aske <vetle.aske@nhn.no>
2023-08-21 13:58:27 +0200

    Removed stream client, no longer a valid example

--- RabbitMq_Stream_integration/BackgroundServices/InitialPopulationData.cs
+++
@@ -1,7 +0,0 @@
-namespace RabbitMq_Stream_integration.BackgroundServices;
-
-public class InitialPopulationData
-{
-    public bool PerformInitialPopulation { get; set; }
-    public DateTime StartTime { get; set; }
-}
